ad5f382889c5565d975cef011b33fe6ca0051dca
Cameron Eldridge <cameldridge@gmail.com>
2019-10-27 13:25:16 -0400

    Add a nicer error when trying to open a non-existing project

--- src/commands/open/mod.rs
+++ src/commands/open/mod.rs
@@ -3,13 +3,14 @@ use pancurses::{ALL_MOUSE_EVENTS, initscr, curs_set, endwin, mousemask, noecho, 
 use crate::model::GitProject;
 
 pub fn open() -> Result<(), Box<dyn Error>> {
+    let mut git_project = GitProject::open()?;
+
     let window = initscr();
     window.keypad(true);
     curs_set(0);
     mousemask(ALL_MOUSE_EVENTS, std::ptr::null_mut());
     noecho();
 
-    let mut git_project = GitProject::open()?;
 
     loop {
         match window.getch() {
--- src/model/git_project.rs
+++ src/model/git_project.rs
@@ -1,10 +1,20 @@
 use std::env::current_dir;
 use std::error::Error;
+use std::fmt::{self, Display, Formatter};
 use std::fs::read_to_string;
 use git2::Repository;
 use serde::{Serialize, Deserialize};
 use super::Project;
 use crate::PROJECT_FILE_NAME;
+
+#[derive(Debug)]
+struct OpenError;
+impl Display for OpenError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{} file does not exist. Did you forget to initialize a project in this repository?", PROJECT_FILE_NAME)
+    }
+}
+impl Error for OpenError {}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct GitProject {
@@ -20,8 +30,10 @@ impl GitProject {
         let repository = Repository::discover(current_dir()?)?;
         let workdir = repository.workdir().unwrap();
         let root = workdir.join(PROJECT_FILE_NAME);
+        if !root.exists() { return Err(Box::new(OpenError)) }
         let string = read_to_string(root)?;
         let git_project = toml::from_str(&string)?;
+
         Ok(git_project)
     }
 
