3a44060d23a3fd06824917c9f2eab229dceb4120
Cameron Eldridge <cameldridge@gmail.com>
2019-10-27 23:56:13 -0400

    Switch to cursive because its more fun

--- src/commands/open/mod.rs
+++ src/commands/open/mod.rs
@@ -1,126 +1,43 @@
 use std::error::Error;
-use pancurses::{ALL_MOUSE_EVENTS, initscr, curs_set, endwin, mousemask, noecho, resize_term, Input, Window};
+use cursive::Cursive;
+use cursive::views::*;
+use cursive::traits::*;
 use crate::model::GitProject;
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 struct State {
-    cmd_typed: String,
-    status: String,
-}
-
-impl State {
-    fn set_cmd_typed<I: AsRef<str>>(&mut self, cmd_typed: I) {
-        self.cmd_typed = cmd_typed.as_ref().to_string();
-    }
-
-    fn set_status<I: AsRef<str>>(&mut self, status: I) {
-        self.status = status.as_ref().to_string();
-    }
-
-    fn draw(&self, git_project: &GitProject, window: &Window) {
-        let (height, width) = window.get_max_yx();
-        let status_line = format!("{: <2$} {: <8}", self.status, self.cmd_typed, width as usize - 11);
-        window.mvprintw(height - 1, 1, status_line);
-        window.refresh();
-    }
+    git_project: GitProject,
 }
 
 pub fn open() -> Result<(), Box<dyn Error>> {
-    let mut git_project = GitProject::open()?;
+    let git_project = GitProject::open()?;
+    let state = State::new(git_project);
+    state.run()
+}
 
-    let window = initscr();
-    window.keypad(true);
-    curs_set(0);
-    mousemask(ALL_MOUSE_EVENTS, std::ptr::null_mut());
-    noecho();
-
-    match run(git_project, &window) {
-        Ok(()) => { endwin(); }
-        error => {
-            endwin();
-            return error
-        }
+impl State {
+    fn new(git_project: GitProject) -> Self {
+        Self { git_project }
     }
 
-    Ok(())
-}
-
-fn run(git_project: GitProject, window: &Window) -> Result<(), Box<dyn Error>> {
-    let mut state = State::default();
-    loop {
-        match window.getch() {
-            Some(Input::Character('q')) => { quit(&mut state, &git_project, window)?; break }
-            Some(Input::Character('w')) => { save(&mut state, &git_project, window)?; }
-            Some(Input::Character('Q')) => if force_quit(&mut state, &git_project, window) { break },
-            Some(Input::Character('?')) => { show_help(&mut state, &git_project, window); }
-            Some(Input::KeyResize) => { resize_term(0, 0); }
-            _ => {}
-        }
+    fn run(self) -> Result<(), Box<dyn Error>> {
+        let mut siv = Cursive::default();
+        siv.add_global_callback('?', show_help);
+        siv.run();
+        Ok(())
     }
-    Ok(())
 }
 
-fn show_help(state: &mut State, git_project: &GitProject, window: &Window) {
-    window.mvprintw(1, 0, r#"
-    Help: git-project UI commands
-    When finished, press any key to close this page.
+fn show_help(siv: &mut Cursive) {
+    let help_text = LinearLayout::vertical()
+        .child(TextView::new("?: Show this help"));
 
-    ?
-        Show this help
-    q
-        Close the git-project UI, saving all changes.
-    QQ
-        Close the git-project UI, discarding all changes.
-    w
-        Save changes without quitting.
-    "#);
+    let dialog = Dialog::around(help_text)
+        .title("Help")
+        .button("Ok", |s| { s.pop_layer(); });
 
-    loop {
-        window.refresh();
-        match window.getch() {
-            Some(Input::KeyResize) => { resize_term(0, 0); }
-            Some(Input::Character(..)) => break,
-            _ => {},
-        }
-    }
+    let event_handler = OnEventView::new(dialog)
+        .on_event('?', Cursive::noop);
 
-    window.clear();
-    state.set_cmd_typed("?");
-    state.draw(git_project, window);
-}
-
-fn quit(state: &mut State, git_project: &GitProject, window: &Window) -> Result<(), Box<dyn Error>> {
-    state.set_cmd_typed("q");
-    state.set_status("Saving...");
-    state.draw(&git_project, window);
-    git_project.save()?;
-    Ok(())
-}
-
-fn save(state: &mut State, git_project: &GitProject, window: &Window) -> Result<(), Box<dyn Error>> {
-    state.set_cmd_typed("w");
-    state.set_status("Saving...");
-    state.draw(&git_project, window);
-    git_project.save()?;
-    state.set_status("Saved!");
-    state.draw(&git_project, window);
-    Ok(())
-}
-
-fn force_quit(state: &mut State, git_project: &GitProject, window: &Window) -> bool {
-    state.set_status("Really discard all changes and quit? Press Q to force quit.");
-    state.set_cmd_typed("Q_");
-    loop {
-        state.draw(&git_project, window);
-        match window.getch() {
-            Some(Input::Character('Q')) => break true,
-            Some(Input::KeyResize) => { resize_term(0, 0); }
-            _ => {
-                state.set_status("Aborted.");
-                state.set_cmd_typed("");
-                state.draw(git_project, window);
-                break false
-            }
-        }
-    }
+    siv.add_layer(event_handler);
 }
